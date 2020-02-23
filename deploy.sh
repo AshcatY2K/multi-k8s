c2a377220c9bbfb9e3fb1c44b24babe2f2486225
Ruan Erasmus <erasmus_ruan@yahoo.com>
2020-02-23 16:40:55 +0200

    fix docker id in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-docker build -t igdevloper/multi-client:latest -t igdevloper/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t igdevloper/multi-server:latest -t igdevloper/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t igdevloper/multi-worker:latest -t igdevloper/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t igdeveloper/multi-client:latest -t igdeveloper/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t igdeveloper/multi-server:latest -t igdeveloper/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t igdeveloper/multi-worker:latest -t igdeveloper/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push igdevloper/multi-client:latest
-docker push igdevloper/multi-server:latest
-docker push igdevloper/multi-worker:latest
+docker push igdeveloper/multi-client:latest
+docker push igdeveloper/multi-server:latest
+docker push igdeveloper/multi-worker:latest
 
-docker push igdevloper/multi-client:$SHA
-docker push igdevloper/multi-server:$SHA
-docker push igdevloper/multi-worker:$SHA
+docker push igdeveloper/multi-client:$SHA
+docker push igdeveloper/multi-server:$SHA
+docker push igdeveloper/multi-worker:$SHA
 
 kubectl apply -f ./k8s
 
-kubectl set image deployment/client-deployment client=igdevloper/multi-client$SHA
-kubectl set image deployment/server-deployment server=igdevloper/multi-server$SHA
-kubectl set image deployment/worker-deployment worker=igdevloper/multi-worker$SHA
+kubectl set image deployment/client-deployment client=igdeveloper/multi-client$SHA
+kubectl set image deployment/server-deployment server=igdeveloper/multi-server$SHA
+kubectl set image deployment/worker-deployment worker=igdeveloper/multi-worker$SHA
